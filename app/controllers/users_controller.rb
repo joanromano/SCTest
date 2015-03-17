a051d7d8ad7daef3712f2f40daeec40521c352f3
Joan Romano <joanromano@gmail.com>
2015-03-17 01:07:49 +0100

    Filter by artist

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,28 +1,40 @@
 class UsersController < ApplicationController
 
   def index
-  	directory = 'users.json'
-    @users_per_page = 10
-  	@data = File.exist?(directory) ? File.read(directory) : '[]'
-    @data = JSON.parse(@data)
-    page = (params[:page] || 0).to_i
-    page = page < 2 ? 0 : page - 1
-    first_degree = user_follows(params[:username])
-    second_degree = []
-    first_degree.each{|username| second_degree.concat(user_follows(username))}
-    second_degree = second_degree.uniq
+	directory = 'users.json'
+	@users_per_page = 10
+	@data = File.exist?(directory) ? File.read(directory) : '[]'
+	@data = JSON.parse(@data)
+	page = get_page((params[:page] || 0).to_i)
+	first_degree = user_follows(params[:username]).map{|username| find_user(username)}.select{|user| user && user['upload_track_count'] > 0}
+	second_degree = user_follows_second_degree(params[:username]).map{|username| find_user(username)}.select{|user| user && user['upload_track_count'] > 0}
 
-  	render :json => [paginate(first_degree, page), paginate(second_degree, page)]
+	render :json => [paginate(first_degree, page), paginate(second_degree, page)]
   end
 
   def paginate(users, page)
-    users.slice(page * @users_per_page, @users_per_page) || []
+	users.slice(page * @users_per_page, @users_per_page) || []
   end
 
-  # TODO FILTER BY ARTIST
+  def get_page(page)
+  	page < 2 ? 0 : page - 1
+  end
+
+  def find_user(username)
+  	data = @data.select{|user| user['username'] == username}
+  	data[0] ? data[0] : nil
+  end
 
   def user_follows(username)
-    user = @data.find_all{|item| item["username"] == username}
-    user[0] ? user[0]["following"] : []
+	user = find_user(username)
+	user ? user['following'] : []
   end
+
+  def user_follows_second_degree(username)
+  	second_degree = []
+  	following = user_follows(username)
+  	second_degree = following.each{|username| second_degree.concat(user_follows(username))}
+  	second_degree.uniq
+  end
+
 end
